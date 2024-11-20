07c35fa30e741a39544216bf30b037d11942df46
CMRicardo <rcorrales@grupocomidas.com>
2024-11-20 14:21:13 -0600

    Implement basic server setup with Express and Socket.IO

---
+++ src/models/server.ts
@@ -0,0 +1,37 @@
+import { createServer, type Server as ServerType } from "node:http";
+import path from "node:path";
+
+import express from "express";
+import { Server as SocketIOServer } from "socket.io";
+
+import { Sockets } from "@/models/sockets";
+
+export class Server {
+  app: express.Application;
+  port: number;
+  server: ServerType;
+  io: SocketIOServer;
+  constructor() {
+    this.app = express();
+    this.port = Number(process.env.PORT) ?? 3000;
+    // HTTP Server
+    this.server = createServer(this.app);
+    // Sockets config
+    this.io = new SocketIOServer(this.server);
+  }
+  middlewares() {
+    this.app.use(express.static(path.resolve(__dirname, "../../public")));
+  }
+
+  configSockets() {
+    new Sockets(this.io);
+  }
+
+  execute() {
+    this.middlewares();
+    this.configSockets();
+    this.server.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+}
